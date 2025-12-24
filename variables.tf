c384387e770e4ce06df9fe9326bc53179092c090
Wasim Ahmad <wasim@local.com>
2025-12-24 15:09:04 +0530

    Github Actions OIDC configuration - 3

--- variables.tf
+++ variables.tf
@@ -1,15 +1,3 @@
 variable "aws_region" {
   default = "ap-south-1"
 }
-
-variable "github_org" {
-  default = "ahmadwasim7"
-}
-
-variable "github_repo" {
-  default = "terraform-aws-oidc"
-}
-
-variable "github_branch" {
-  default = "master"
-}
